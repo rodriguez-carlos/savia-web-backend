68596e44236f617da45021df17ce119ca7b4f319
unknown <carlosdanielrod@gmail.com>
2022-03-25 20:43:02 -0500

    added content-type schopera

---
+++ src/api/schopera/controllers/schopera.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  schopera controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::schopera.schopera');
---
+++ src/api/schopera/routes/schopera.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * schopera router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::schopera.schopera');
